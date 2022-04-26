4c328865ab4047905eaffa6a9605629f689c2e17
Vyslobitskyi <vyslobitskyi@qarea.biz>
2022-04-26 18:09:55 +0300

    catch error

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Body, Controller, Delete, Get, InternalServerErrorException, Param, Post, Put } from '@nestjs/common';
+
 import { UsersService } from './users.service';
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { User } from './user.schema';
 
 @Controller('users')
@@ -9,26 +10,36 @@ export class UsersController {
 
     @Get()
     findAll(): Observable<User[]> {
-        return this._users.findAll();
+        return this._users.findAll().pipe(
+            catchError((e) => throwError(() => new InternalServerErrorException(e)))
+        );
     }
 
     @Get(':id')
     findById(@Param('id') id: string): Observable<User> {
-        return this._users.findById(id);
+        return this._users.findById(id).pipe(
+            catchError((e) => throwError(() => new InternalServerErrorException(e)))
+        );
     }
 
     @Post()
     create(@Body() dto: any): Observable<User> {
-        return this._users.create(dto);
+        return this._users.create(dto).pipe(
+            catchError((e) => throwError(() => new InternalServerErrorException(e)))
+        );
     }
 
     @Put()
     findByIdAndUpdate(id: string, dto: any): Observable<User> {
-        return this._users.findByIdAndUpdate(id, dto);
+        return this._users.findByIdAndUpdate(id, dto).pipe(
+            catchError((e) => throwError(() => new InternalServerErrorException(e)))
+        );
     }
 
     @Delete()
     findByIdAndRemove(id: string): Observable<User> {
-        return this._users.findByIdAndRemove(id);
+        return this._users.findByIdAndRemove(id).pipe(
+            catchError((e) => throwError(() => new InternalServerErrorException(e)))
+        );
     }
 }
